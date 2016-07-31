1df31186b2606266e406c3af7388e26b6a22c462
oakwoodgates <funkyoaktree@gmail.com>
2016-07-31 18:04:20 -0400

    rewrite for Wontrapi

--- includes/class-ftr-signup-to-ontraport.php
+++ includes/class-ftr-signup-to-ontraport.php
@@ -39,62 +39,79 @@ class ZB_FTR_Signup_to_ONTRAPORT {
 	 * @return void
 	 */
 	public function hooks() {
-		add_action('user_register', array( $this, 'conditions' ), 10, 1);
+		add_action('user_register', array( $this, 'create_ftr_user_entry' ), 99, 1);
+	//	add_action('op_test', array( $this, 'create_ftr_user_entry' ), 10, 1);
 	}
 
 	/**
-	 * Check conditions to see if we should setup our
-	 * data and preform api_call()
-	 * @since  1.0.2
-	 * @param  [type] $user_id [description]
-	 * @return [type]          [description]
+	 * Adds FTR User to Ontraport with FTR Customer tag and Newsletter sequence
+	 * @param  [type] $uid User ID
+	 * @return [type]      [description]
+	 * @since  1.0.4
 	 */
-	public function conditions( $user_id ) {
-
-		if ( ! $user_id ) {
+	function create_ftr_user_entry( $uid ) {
+		if ( empty( $uid ) ) {
 			return;
 		}
 
-		$user = get_user_by( 'id', $user_id );
+		$u = get_user_by( 'id', $uid );
 
-		if ( ! in_array( 'ftr_user', $user->roles ) ){
+		if ( ! is_object( $u ) ) {
 			return;
 		}
 
-		self::setup_data( $user );
-	}
+		// check for ftr user. should probably filter conditions longterm
+		if ( ! in_array( 'ftr_user', $u->roles ) ){
+			return;
+		}
 
-	/**
-	 * Original function from PilotPress->add_new_register_user_to_ONTRAPORT().
-	 * Setup the user data to send to Ontraport
-	 *
-	 * @param  [type] $user [description]
-	 * @since  1.0.2
-	 * @return [type]       [description]
-	 */
-	function setup_data( $user ) {
+		// will probably be using this when registering wp users,
+		// so don't expect this to be true
+		$op_id = ( ! empty( get_user_meta( $uid, 'wontrapi_id', true ) ) ) ? get_user_meta( $uid, 'wontrapi_id', true ) : '';
 
-		$userData = array(
-			"firstname"=>$user->user_firstname,
-			"lastname"=>$user->user_lastname,
-			"email"=>$user->user_email,
-			"tags"=>'FTR Customer',
-			"sequences"=>'1'
-		);
+		if ( ! $op_id ) {
+			$data = array(
+				'email'	=> $u->user_email,
+			);
 
-		self::add_newly_registered_contact_api_call( $userData );
-	}
+			// Update/Create a contact if the email record
+			// does/not exist in Ontraport
+			$op = wontrapi_update_or_create_contact( $u->user_email, $data );
 
-	/**
-	 * Start PilotPress's api call
-	 * @since  1.0.2
-	 * @param [type] $userData [description]
-	 */
-	function add_newly_registered_contact_api_call( $userData ) {
+			// if the contact was created new in Ontraport,
+			// get the id of the new user from OP
+			$op_id = ( ! empty( $op->data->id ) ) ? $op->data->id : $op_id;
+		}
 
-		global $pilotpress;
+		// if contact was updated (or failed?) it
+		// does not return the id from Ontraport
+		if ( ! $op_id ){
+			$op = '';
+			// get the contact from OP by email
+			$op = wontrapi_get_contacts_by( 'email', $u->user_email );
+			// get the id of the user in OP
+			if ( ! empty( $op->data[0]->id ) ){
+				$op_id = $op->data[0]->id;
+			} else {
+				// something messed up
+				return;
+			}
+		}
 
-		$api_result = $pilotpress->api_call( 'add_newly_registered_contact', $userData );
+		if ( ! empty( $u->user_firstname ) ) {
+			$abc = wontrapi_get_contact( $op_id );
+			if ( empty( $abc->firstname ) ) {
+				$abc->firstname = $u->user_firstname;
+				wontrapi_update_contact( $abc );
+			}
+		}
+
+		// update the user meta in WP with the id from OP
+		update_user_meta( $uid, 'wontrapi_id', $op_id );
+		// add tags for ftr user
+		wontrapi_add_tags_to_contacts( array( $op_id ), array( '587' ) );
+		// add newsletter sequence
+		wontrapi_add_sequence_to_contact( $op_id, '1' );
 	}
 
 }
